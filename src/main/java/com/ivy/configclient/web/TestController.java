ad478891243d75068ed6da5ab2983be0a5ca4220
Ivy Xue <i.xue@sap.com>
2019-02-20 20:56:03 +0800

    initial

---
+++ src/main/java/com/ivy/configclient/web/TestController.java
@@ -0,0 +1,19 @@
+package com.ivy.configclient.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class TestController {
+
+    @Value("${info.from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+}
